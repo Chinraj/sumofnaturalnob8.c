5a86c95cb0b26c8493b8e322c29272ffb2d7961c
Chinraj.K <chinrajkalimuthu@gmail.com>
2016-07-05 10:59:37 +0530

    Create sumofnaturaknob8.c

---
+++ sumofnaturaknob8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("Enter number  :");
+scanf("\%d",&a);
+b=a*(a+1)/2;
+printf("%d is a sum of natural ",b);
+getch();
+}
